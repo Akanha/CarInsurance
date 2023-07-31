142fce2d17771055c9c72c8845f0e8b38c12a2b1
Akanha <ashirvadmishra17@gmail.com>
2023-07-31 18:05:45 +0530

    "all done UI remains"

---
+++ src/app/invoice/invoice.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-invoice',
+  templateUrl: './invoice.component.html',
+  styleUrls: ['./invoice.component.css']
+})
+export class InvoiceComponent {
+  email:any=localStorage.getItem("email")
+  policy:any=localStorage.getItem("policy")
+  regNo:any=localStorage.getItem("regNo")
+
+}
+
+
